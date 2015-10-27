546793907812a03cd87801cbd3448ea70293253f
Alexandr Okhotnikov <alx@bars-open.ru>
2015-10-27 16:53:19 +0300

    Initial Commit

---
+++ iiiimmmm2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iiiimmmm2
+//
+//  Created by Alexandr Okhotnikov on 27.10.15.
+//  Copyright © 2015 АО "БАРС Груп". All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iiiimmmm2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iiiimmmm2
+//
+//  Created by Alexandr Okhotnikov on 27.10.15.
+//  Copyright © 2015 АО "БАРС Груп". All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
